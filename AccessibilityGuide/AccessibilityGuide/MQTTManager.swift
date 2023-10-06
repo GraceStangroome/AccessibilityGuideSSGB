4ebfd3a86e1b4abc329f22f0917f88f1549d5961
Grace <stangroomegrace@gmail.com>
2023-10-06 19:02:02 +0100

    Trying to get MQTT to work and failing

--- AccessibilityGuide/AccessibilityGuide/MQTTManager.swift
+++ AccessibilityGuide/AccessibilityGuide/MQTTManager.swift
@@ -10,7 +10,7 @@ import CocoaMQTT
 
 class MQTTManager {
     
-    let mqtt = CocoaMQTT(clientID: "iPad", host: "192.169.0.12", port: 1883)
+    let mqtt = CocoaMQTT(clientID: "iPad", host: "raspberrypi.local", port: 1883)
     
     init() {
         configureMQTT()
@@ -21,36 +21,113 @@ class MQTTManager {
         mqtt.logLevel = .debug
         mqtt.username = "iPad"
         mqtt.password = "Apple!"
+        let message = "will dieout"
+        let payload: [UInt8] = Array(message.utf8)
+        mqtt.willMessage = CocoaMQTTMessage(topic: "messagesFromiPad", payload: payload)
         mqtt.keepAlive = 60
         mqtt.autoReconnect = true
         mqtt.autoReconnectTimeInterval = 5
-        
+        mqtt.delegate = self
     }
     
-    // successful connection
-    func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
-        print("Connected to \(host) on port \(port)")
+    func connect() {
+        let result = mqtt.connect()
+        print("Connection was successful? \(result)")
     }
     
-    // unsuccessful connection
-    func mqtt(_ mqtt: CocoaMQTT, withError err: Error?) {
-        if let error = err {
-            print("Disconnected with error: \(error.localizedDescription)")
-        } else {
-            print("Disconnected")
+    func subscribe(toTopic topic: String) {
+        mqtt.subscribe(topic)
+        print("Subscribed to topic: \(topic)")
+    }
+    
+    func publish(topic: String, content: String) {
+        let array : [UInt8] = Array(content.utf8)
+        let message = CocoaMQTTMessage(topic: topic, payload: array, qos: CocoaMQTTQoS.qos2, retained: true)
+        mqtt.publish(message)
+    }
+}
+    
+extension MQTTManager: CocoaMQTTDelegate {
+    func mqttUrlSession(_ mqtt: CocoaMQTT, didReceiveTrust trust: SecTrust, didReceiveChallenge challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void){
+        if (challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust) {
+            let myCert = "myCert"
+            let certData = Data(base64Encoded: myCert as String)!
+
+            if let trust = challenge.protectionSpace.serverTrust,
+               let cert = SecCertificateCreateWithData(nil,  certData as CFData) {
+                let certs = [cert]
+                SecTrustSetAnchorCertificates(trust, certs as CFArray)
+
+                completionHandler(URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust: trust))
+                return
+            }
         }
-        connect()
+
+        completionHandler(URLSession.AuthChallengeDisposition.cancelAuthenticationChallenge, nil)
+
     }
-    
-    func mqtt(_ mqtt: CocoaMQTT, didRecieveMessage message: CocoaMQTTMessage, id: UInt16) {
+
+
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        TRACE("ack: \(ack)")
+        func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+            switch ack {
+            case .accept:
+                print("Connection accepted. You are now connected to the broker.")
+                mqtt.subscribe("messagesforiPad", qos: CocoaMQTTQoS.qos1)
+            case .unacceptableProtocolVersion:
+                print("Connection refused: Unacceptable protocol version")
+                // Handle the error appropriately.
+            case .identifierRejected:
+                print("Connection refused: Identifier rejected")
+                // Handle the error appropriately.
+            case .serverUnavailable:
+                print("Connection refused: Server unavailable")
+                // Handle the error appropriately.
+            case .badUsernameOrPassword:
+                print("Connection refused: Bad username or password")
+                // Handle the error appropriately.
+            case .notAuthorized:
+                print("Connection refused: Not authorized")
+                // Handle the error appropriately.
+            case .reserved: // unusure what this case means but it was missing
+                print("Connection refused: Reserved")
+            }
+        }
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didStateChangeTo state: CocoaMQTTConnState) {
+        TRACE("new state: \(state)")
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        if let messageText = message.string {
+            TRACE("message: \(message.string!.description), id: \(id)") // we can force it because the if statement safely checks for it
+        } else {
+            TRACE("empty message, id: \(id)")
+        }
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        TRACE("id: \(id)")
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        print("Recieved a Message")
+        if let messageText = message.string {
+            TRACE("message: \(message.string!.description), id: \(id)") // we can force it because the if statement safely checks for it
+        } else {
+            TRACE("Recieved a message with no text, id: \(id)")
+        }
+        let name = NSNotification.Name(rawValue: "MQTTMessageNotification")
+        NotificationCenter.default.post(name: name, object: self, userInfo: ["message": message.string!, "topic": message.topic, "id": id])
+        
         if let messageString = message.string {
             print("Message recieved in topic \(message.topic) with payload \(message.string!)")
             if (message.string == "lowerdeck") {
                 print("Trying to go to lowerdeck")
                 goToLowerDeck()
-            }
-            
-            else if (message.string == "lowerdeck1") {
+            } else if (message.string == "lowerdeck1") {
                 print("Trying the pop up")
                 var popUpWindow: PopUpWindow!
                 popUpWindow = PopUpWindow(title: "You are now on the Lower Deck", text: "This is the last level of the ship’s interior. The SS Great Britain being launched into Bristol’s Floating Harbour on 19 July 1843. Even Prince Albert came to Bristol to celebrate.")
@@ -58,8 +135,25 @@ class MQTTManager {
             }
         }
     }
-    
+
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopics success: NSDictionary, failed: [String]) {
+        TRACE("subscribed: \(success), failed: \(failed)")
+    }
+
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopics topics: [String]) {
+        TRACE("topic: \(topics)")
+    }
+
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        TRACE()
+    }
+
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        TRACE()
+    }
+
     func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        TRACE("\(String(describing: err))")
         let maxReconnectAttempts = 10
         var reconnectAttempts = 0
         if reconnectAttempts < maxReconnectAttempts {
@@ -72,20 +166,23 @@ class MQTTManager {
             print("Reached maximum reconnection attempts.")
         }
     }
-    
-    func connect() {
-        let result = mqtt.connect()
-        print("Connection was successful? \(result)")
-    }
-    
-    func subscribe(toTopic topic: String) {
-        mqtt.subscribe(topic)
-    }
-    
-    func publish(topic: String, content: String) {
-        let array : [UInt8] = Array(content.utf8)
-        let message = CocoaMQTTMessage(topic: topic, payload: array, qos: CocoaMQTTQoS.qos2, retained: true)
-        mqtt.publish(message)
+}
+
+extension MQTTManager {
+    func TRACE(_ message: String = "", fun: String = #function) {
+        let names = fun.components(separatedBy: ":")
+        var prettyName: String
+        if names.count == 2 {
+            prettyName = names[0]
+        } else {
+            prettyName = names[1]
+        }
+        
+        if fun == "mqttDidDisconnect(_:withError:)" {
+            prettyName = "didDisconnect"
+        }
+
+        print("[TRACE] [\(prettyName)]: \(message)")
     }
 }
 
